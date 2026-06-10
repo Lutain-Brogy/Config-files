7a6e8a5d6e961fc951cd7b7f3d4d1fa46e67bc41
Lutain-Brogy <brogylutain@gmail.com>
2026-06-10 03:52:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 st.title(".env file writer") 
 
 user_input = st.text_input
-choose_selection = st.selectbox('Chooose the type of file you want to write today'
-                      [".env",
-                       "none"
-                      ]
-                     )
+choose_selection = st.selectbox(
+    'Choose the type of file you want to write today',
+    [".env", "none"]
+)
 
-if choose_selection = '.env':
-st.write('Hello')
+if choose_selection == '.env':
+    st.write('Hello')
