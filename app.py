f7631d0a5baf1f2976cf3e60364c34b4e8be32ee
Lutain-Brogy <brogylutain@gmail.com>
2026-06-10 03:46:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,11 @@ import streamlit as st
 st.title(".env file writer") 
 
 user_input = st.text_input
+
+choose mode = st.text_input
+
+if choose mode:
+ mode = "env. file making"
+else:
+  mode = 'normal'
+  
