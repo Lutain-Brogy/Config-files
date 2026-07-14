4bb7e3ff029eb56b779a7d2a803c8a04ec5ddb84
Lutain-Brogy <brogylutain@gmail.com>
2026-07-14 07:49:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,3 @@
 import streamlit as st
-st.title(".env file writer") 
 
-user_input = st.text_input
-choose_selection = st.selectbox(
-    'Choose the type of file you want to write today',
-    [".env", "none"]
-)
-
-if choose_selection == '.env':
-    env_selection = st.selectbox(
-        'Choose the .env file type you want',
-        ['Ai API .env type']
-    )
-
-    if env_selection == 'Ai API .env type':
-        A = user_input('Type the defualt provider')
-
-        st.code(f'''
-# Default provider
-DEFAULT_PROVIDER={A} 
-''')
-        
+st.title("Firestore Mini RuleSense")
