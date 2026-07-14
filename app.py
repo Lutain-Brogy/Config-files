fe50770b4074e416da2a6ba486cdd032e6782858
Lutain-Brogy <brogylutain@gmail.com>
2026-07-14 08:03:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
 st.title("Firestore Mini RuleSense")
+
+mode = st.selectbox('Choose',
+                    ["Rule","Learn"]
+if mode == 'Rule':
+
+sentence = st.text_input("Enter sentence")
+
+match = re.search(r"At collection (\w+) and document (\w+)", sentence)
+
+if match:
+    A = match.group(1)
+    B = match.group(2)
+
+    rule = f"""
+match /{A}/{B} {{
+  allow read: if request.auth != null;
+}}
+"""
+    st.code(rule, language="javascript")
