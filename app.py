75519e157367112b2b02e627fe6fd466c938c4b2
Lutain-Brogy <brogylutain@gmail.com>
2026-07-14 10:13:53 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,22 +4,55 @@ import re
 st.title("Firestore Mini RuleSense")
 
 mode = st.selectbox(
-    'Choose',
-    ["Rule", "Learn"]
+    'Operation type',
+    ["Standard", "Learning"]
 )
 
-if mode == 'Rule':
-    sentence = st.text_input("Enter sentence")
+if mode == 'Standard':
 
-    match = re.search(r"At collection (\w+) and document (\w+)", sentence)
+    Rule = st.selectbox(
+        'What to do today?',
+        ["Public rule", "Authenticated rule", "Create role", "Time based rule"]
+    )
 
-    if match:
-        A = match.group(1)
-        B = match.group(2)
+    if Rule == "Public rule":
 
-        rule = f"""
-match /{A}/{B} {{
-  allow read: if request.auth != null;
+        sentence = st.text_input(
+            "Write your rule sentence:",
+            "At collection users and document profile, allow read for everybody"
+        )
+
+        match = re.search(
+            r"At collection (\w+) and document (\w+), (\w+) (\w+) for everybody",
+            sentence
+        )
+
+        if match:
+
+            A = match.group(1)  # collection
+            B = match.group(2)  # document
+            C = match.group(3)  # allow/deny
+            D = match.group(4)  # read/write
+
+
+            if C == "allow":
+                condition = "true"
+            elif C == "deny":
+                condition = "false"
+
+
+            rule = f"""
+rules_version = '2';
+
+service cloud.firestore {{
+  match /databases/{{database}}/documents {{
+
+    match /{A}/{B}/{{any}} {{
+      allow {D}: if {condition};
+    }}
+
+  }}
 }}
 """
-        st.code(rule, language="javascript")
+
+            st.code(rule, language="javascript")
