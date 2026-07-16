67635038ddb664afa6fb220b48b12652f2066124
Lutain-Brogy <brogylutain@gmail.com>
2026-07-16 10:11:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -104,19 +104,19 @@ if match:
 
 
     elif user_type == "UID":
-        condition = f"&& request.auth != null && request.auth.uid == ''"
+        condition = f"&& request.auth.uid == ''"
 
 
     elif user_type == "Role":
-        condition = f"&& request.auth != null && request.auth.token.role == ''"
+        condition = f"&& request.auth.token.role == ''"
 
 
     elif user_type == "Admin":
-        condition = "&& request.auth != null && request.auth.token.role == 'admin'"
+        condition = "&& request.auth.token.role == 'admin'"
 
 
     elif user_type == "Owner":
-        condition = "&& request.auth != null && request.auth.uid == resource.data.ownerId"
+        condition = "&& request.auth.uid == resource.data.ownerId"
 
 
 
